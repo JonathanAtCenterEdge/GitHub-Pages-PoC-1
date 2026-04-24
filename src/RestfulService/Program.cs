d247dcc92a64aa6dcc842ab1468b851cdadf1f35
Jonathan Richards <91483632+JonathanAtCenterEdge@users.noreply.github.com>
2026-04-24 13:49:06 -0400

    adjust

--- src/RestfulService/Program.cs
+++ src/RestfulService/Program.cs
@@ -46,7 +46,7 @@ app.MapGet("/items", () =>
 .WithTags("Items")
 .WithName("GetItems");
 
-app.MapPost("/items", (ItemDto item) =>
+app.MapPost("/items/new", (ItemDto item) =>
 {
     return Results.Created($"/items/{item.Id}", item);
 })
